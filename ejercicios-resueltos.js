31f9fea1c2cda8e09b18c4709c295516b22ff566
Javier <jaguiro@gmail.com>
2019-10-24 13:23:36 +0200

    Actualización 24.10.2019

--- ejercicios-resueltos.js
+++ ejercicios-resueltos.js
@@ -2,6 +2,8 @@
 
 // First Reverse
 
+// Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+
 function reverseString(str) {
   let newString = "";
   for (let i = str.length - 1; i >= 0; i--) {
@@ -12,7 +14,12 @@ function reverseString(str) {
 
 reverseString('Javier');
 
+
 // First Factorial
+
+// Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it. For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+
+Use the Parameter Testing feature in the box below to test your code with different arguments.
 
 function factorial(n) {
   let result = 1;
@@ -24,7 +31,10 @@ function factorial(n) {
 
 factorial(5)
 
+
 // Longest Word
+
+// Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty.
 
 function longestWord(string) {
   let str = string.split(" ");
@@ -43,7 +53,10 @@ console.log(longestWord('¿Cuál de estas palabras es la más larga?'));
 
 // Al parecer no es correcto porque sólo deben tomarse las palabras y no los símbolos.
 
+
 // Simple Adding
+
+// Have the function SimpleAdding(num) add up all the numbers from 1 to num. For example: if the input is 4 then your program should return 10 because 1 + 2 + 3 + 4 = 10. For the test cases, the parameter num will be any number from 1 to 1000.
 
 function Adding(n) {
   let result = 0;
@@ -54,6 +67,7 @@ function Adding(n) {
 }
 
 console.log(Adding(5));
+
 
 // Letter Capitalize
 
@@ -72,11 +86,10 @@ function LetterCapitalize(str) {
 }
 LetterCapitalize("Argument goes here");
 
+
 // Check Nums
 
 // Have the function CheckNums(num1,num2) take both parameters being passed and return the string true if num2 is greater than num1, otherwise return the string false. If the parameter values are equal to each other then return the string -1.
-
-// Use the Parameter Testing feature in the box below to test your code with different arguments.
 
 function CheckNums(num1, num2) {
   if (num2 > num1) {
@@ -91,6 +104,7 @@ function CheckNums(num1, num2) {
 }
 
 CheckNums(readline());
+
 
 // Time Convert
 
@@ -115,3 +129,43 @@ function TimeConvert(num) {
 
 console.log(TimeConvert(number))  // 1:40
 
+
+// Alphabet Soup
+
+// Have the function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in alphabetical order(ie.hello becomes ehllo).Assume numbers and punctuation symbols will not be included in the string.
+
+// Examples
+// Input: "coderbyte"
+// Output: bcdeeorty
+// Input: "hooplah"
+// Output: ahhloop
+
+const word = "javier"
+
+function AlphabetSoup(str) {
+  let arraySplit = str.split("");
+  console.log(arraySplit)
+  let arraySort = arraySplit.sort();
+  let arrayJoin = arraySort.join("");
+  return arrayJoin;
+}
+
+console.log(AlphabetSoup(word))
+
+
+// Letter Changes
+
+// Have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string.
+
+// Examples
+// Input: "hello*3"
+// Output: Ifmmp * 3
+// Input: "fun times!"
+// Output: gvO Ujnft!
+
+function LetterChanges(str) {
+
+  return str
+}
+
+console.log 
---
+++ main.js
@@ -0,0 +1,42 @@
+function add(n1, n2) {
+    console.log(n1 + n2);
+}
+add(3, 2);
+
+
+
+/*
+Haga que la función LetterChanges(str) tome el parámetro str que se está pasando y lo modifique utilizando el siguiente algoritmo. Reemplace cada letra de la cadena con la letra que le sigue en el alfabeto (es decir, c se convierte en d, z se convierte en a). Luego escribe en mayúsculas cada vocal en esta nueva cadena(a, e, i, o, u) y finalmente devuelve esta cadena modificada.
+*/
+
+// Examples
+
+// Input: "hello*3"
+// Output: Ifmmp*3
+
+// Input: "fun times!"
+// Output: gvO Ujnft!
+
+const str = 'hello*3'
+
+function LetterChanges(str) {
+    
+    return str
+}
+
+LetterChanges(str)
+
+//=========================================
+
+// function uppCaseMe() {
+//     return arguments[0].toUpperCase();
+// }
+
+// myString = "a string like no other"
+
+// myNewString = myString.replace(/[aeiou]/g, uppCaseMe);
+
+// console.log(myString);
+// console.log(myNewString)
+
+//==========================================
